f5e90aaabc28a73c17a36d7e083b7fd6c71ed299
Shanmugapriyan <agsshanmu@gmail.com>
2024-06-29 15:30:49 +0530

    Static methods
    added oop

---
+++ OOP/static/Main.java
@@ -0,0 +1,47 @@
+
+public class Main{
+    static int Rollno = 14;//static
+    int salary = 140;
+
+    //constructor
+//    public Main(int salary){
+//        this.salary = salary; //this represents the current instance inside the method
+//    }
+    
+    //constructor overloading 
+//    public Main(int rollno , int salary){
+//        this.Rollno = rollno;
+//        this.salary = salary;
+//    }
+    static void fun(){
+        System.out.println("This is a static method");
+        //greetings(); Inside the static method non-static method can not be used
+    }
+    void greetings(){
+        System.out.println("This is instance object");
+        fun();//inside the non-static method static can be used.
+    }
+
+    //static block
+    static int number1 = 1;
+    static int number2;
+
+    static {
+        System.out.println("This is  a static block");
+        number2 = number1 + 2;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        System.out.println(Main.Rollno);
+        Main obj = new Main(45);
+        Main obj1 = new Main(45, 4500);
+        System.out.println(obj.salary);
+        System.out.println(obj1.salary);
+        obj.greetings();
+        fun();
+
+        Main stacticobj = new Main();
+        System.out.println(Main.number2 + " "+ Main.number1);
+    }
+}
